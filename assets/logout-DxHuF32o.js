604c5fc29115a080ce6d2a33e2a578b5ebf7a234
shishanx <harry850402@gmail.com>
2026-03-06 03:59:30 +0800

    Updates

---
+++ assets/logout-DxHuF32o.js
@@ -0,0 +1,1 @@
+import{w as o,y as t}from"./chunk-EPOLDU6W-8eE4Auzu.js";function r(){return localStorage.removeItem("token"),t("/")}const u=o(function(){return null});export{r as clientAction,u as default};
